49ed8c61cd0da8763cee2a4447500f47b648b0a7
git-marcopitra <marcomorete73@hotmail.com>
2023-11-22 15:26:56 +0100

    :tada: feat: setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { ThemeManager } from '@/components';
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <ThemeManager>
+    <Component {...pageProps} />
+  </ThemeManager>
+);
+
+export default App;
---
+++ pages/dapp/swap.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react';
+
+const Swap: FC = () => <>Swap</>;
+
+export default Swap;
